6b1a05ae023e014e74972fd210470b558aaf11a5
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-16 20:11:32 +0330

    fixed bugs

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ upBtn.addEventListener("click", slideUp);
 downBtn.addEventListener("click", slideDown);
 
 let slideNum = 0;
-const height = window.innerHeight;
 
 leftSlides.style.top = `-${3 * 100}vh`;
 console.log(height);
@@ -19,8 +18,8 @@ function slideUp() {
     slideNum = 0;
   }
   console.log(slideNum);
-  rightSlides.style.transform = `translateY(-${height * slideNum}px`;
-  leftSlides.style.transform = `translateY(${height * slideNum}px`;
+  rightSlides.style.transform = `translateY(-${100 * slideNum}vh`;
+  leftSlides.style.transform = `translateY(${100 * slideNum}vh`;
 }
 
 function slideDown() {
